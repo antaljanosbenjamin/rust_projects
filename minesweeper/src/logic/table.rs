9821f4adb95c41abbc8236b14902e574a880a1cd
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2019-07-16 22:14:24 +0200

    Make constructing of a Table incremental

--- minesweeper/src/logic/table.rs
+++ minesweeper/src/logic/table.rs
@@ -1,48 +1,83 @@
-use crate::logic::field::{DummyField, Field};
+use crate::logic::field::Field;
 use std::collections::HashSet;
 
 pub struct Table {
-    values: Vec<Vec<Box<dyn Field>>>,
-    number_of_mines: u16
+    width: u8,
+    height: u8,
+    number_of_mines: u16,
+    mine_locations: Option<HashSet<(u8, u8)>>,
+    fields: Option<Vec<Vec<Box<dyn Field>>>>,
 }
 
 impl Table {
-    pub fn new(width: u8, height: u8, number_of_mines: u16) -> Result<Table, &'static str> {
-        let max_number_of_mines = (width as f32 * height as f32 * 0.5) as u16;
-        let min_number_of_mines = (width as f32 * height as f32 * 0.05) as u16;
-        if max_number_of_mines < number_of_mines {
+    fn generate_mine_locations(&mut self) -> Result<(), &'static str> {
+        assert!(self.mine_locations == None);
+        let max_number_of_mines = (self.width as f32 * self.height as f32 * 0.5) as u16;
+        let min_number_of_mines = (self.width as f32 * self.height as f32 * 0.05) as u16;
+        if max_number_of_mines < self.number_of_mines {
             return Err("Too much mines!");
         }
-        if min_number_of_mines > number_of_mines {
+        if min_number_of_mines > self.number_of_mines {
             return Err("Too few mines!");
         }
 
-
-        let mut table = Table{values: Vec::<Vec<Box<dyn Field>>>::new(), number_of_mines};
-        let mut mines_location = HashSet::new();
-        while (mines_location.len() as u16) < number_of_mines {
-            mines_location.insert((rand::random::<u8>() % height, rand::random::<u8>() % width ));
+        let mut mine_location = HashSet::new();
+        while (mine_location.len() as u16) < self.number_of_mines {
+            mine_location.insert((
+                rand::random::<u8>() % self.height,
+                rand::random::<u8>() % self.width,
+            ));
         }
-        for r in 0..height {
+        self.mine_locations = Some(mine_location);
+        Ok(())
+    }
+
+    fn generate_fields(&mut self) -> Result<(), &'static str> {
+        if self.mine_locations.is_none() {
+            return Err("Mine locations are missing!");
+        }
+        let &mine_locations = &self.mine_locations.as_ref().unwrap();
+
+        let mut fields = Vec::<Vec<Box<dyn Field>>>::new();
+        for r in 0..self.height {
             let mut row = Vec::<Box<dyn Field>>::new();
-            for c in 0..width {
-                if  mines_location.contains(&(r,c)) {
+            for c in 0..self.width {
+                if mine_locations.contains(&(r, c)) {
                     row.push(Field::new(true, 0));
                 } else {
                     row.push(Field::new(false, 0))
                 }
             }
-            table.values.push(row);
+            fields.push(row);
         }
+        self.fields = Some(fields);
+        Ok(())
+    }
+
+    pub fn new(width: u8, height: u8, number_of_mines: u16) -> Result<Table, &'static str> {
+        let mut table = Table {
+            width,
+            height,
+            number_of_mines,
+            mine_locations: None,
+            fields: None,
+        };
+        table.generate_mine_locations()?;
+        table.generate_fields()?;
         Ok(table)
     }
 
     pub fn print(&self) {
-        for row in self.values.iter() {
-            for cell in row.iter() {
-                print!("{}", cell.get_char_repr());
+        match &self.fields.as_ref() {
+            Some(fields) => {
+                for row in fields.iter() {
+                    for cell in row.iter() {
+                        print!("{}", cell.get_char_repr());
+                    }
+                    println!("");
+                }
             }
-            println!("");
+            None => println!("Field informations are missing!"),
         }
     }
 }
