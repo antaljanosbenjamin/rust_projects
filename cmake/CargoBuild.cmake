06532055c072205ee4d1753023d298fd36e9577d
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2020-07-05 19:55:39 +0200

    Add both shared and static lib to cmake

--- cmake/CargoBuild.cmake
+++ cmake/CargoBuild.cmake
@@ -13,20 +13,32 @@ function(cargo_build_library LIB_NAME)
   set(CARGO_ARGS build --target-dir ${CARGO_TARGET_DIR}
                  $<$<BOOL:${IS_RELEASE}>:--release>)
 
-  set(LIB_FILE
+  set(STATIC_LIB_FILE
       "${CARGO_TARGET_DIR}/${CARGO_BUILD_TYPE}/${CMAKE_STATIC_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX}"
   )
+  set(SHARED_LIB_FILE
+      "${CARGO_TARGET_DIR}/${CARGO_BUILD_TYPE}/${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+  )
+  set(LIB_FILES "${STATIC_LIB_FILE} ${SHARED_LIB_FILE}")
 
   file(GLOB_RECURSE LIB_SOURCES "*.rs")
 
   add_custom_command(
-    OUTPUT ${LIB_FILE}
+    OUTPUT ${LIB_FILES}
     COMMAND cargo ${CARGO_ARGS}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    DEPENDS ${LIB_SOURCES}
+    DEPENDS ${LIB_SOURCES} ${CMAKE_CURRENT_SOURCE_DIR}/Cargo.toml
     COMMENT "running cargo")
-  add_custom_target(${LIB_NAME}_target ALL DEPENDS ${LIB_FILE})
-  add_library(${LIB_NAME} STATIC IMPORTED GLOBAL)
-  add_dependencies(${LIB_NAME} ${LIB_NAME}_target)
-  set_target_properties(${LIB_NAME} PROPERTIES IMPORTED_LOCATION ${LIB_FILE})
+
+  add_custom_target(${LIB_NAME}_target ALL DEPENDS ${LIB_FILES})
+
+  add_library(${LIB_NAME}_static STATIC IMPORTED GLOBAL)
+  add_dependencies(${LIB_NAME}_static ${LIB_NAME}_target)
+  set_target_properties(${LIB_NAME}_static PROPERTIES IMPORTED_LOCATION
+                                                      ${STATIC_LIB_FILE})
+
+  add_library(${LIB_NAME}_shared STATIC IMPORTED GLOBAL)
+  add_dependencies(${LIB_NAME}_shared ${LIB_NAME}_target)
+  set_target_properties(${LIB_NAME}_shared PROPERTIES IMPORTED_LOCATION
+                                                      ${SHARED_LIB_FILE})
 endfunction()
