2ccbb7a2cc0e736eddaf0711e7e3e85e60c96dfb
János Benjamin Antal <antal.janos.benjamin@gmail.com>
2021-04-04 23:13:48 +0200

    Make FieldInfo public

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,5 +1,5 @@
 mod minesweeper_logic;
 pub use minesweeper_logic::basic_types::SizeType;
-pub use minesweeper_logic::field_info::FieldType;
+pub use minesweeper_logic::field_info::{FieldInfo, FieldState, FieldType};
 pub use minesweeper_logic::game::{Game, GameLevel};
 pub use minesweeper_logic::results::{FlagResult, OpenInfo, OpenResult};
